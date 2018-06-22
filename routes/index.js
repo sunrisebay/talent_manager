ff384a6b97e867f91255a7785d5e10ffe702b427
sunrisebay <mpan@bu.edu>
2018-06-21 22:52:48 -0700

    add dummy postgres connection module

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const pg = require('pg');
+const config = {
+    user: 'postgres',
+    database: 'database',
+    password: 'password',
+    port: 5432
+};
+
+// pool takes the object above -config- as parameter
+const pool = new pg.Pool(config);
+
+
+pool.connect(function (err, client, done) {
+   if (err) {
+       console.log("Can not connect to the DB" + err);
+   }
+   client.query('SELECT * FROM testtable', function (err, result) {
+        done();
+        if (err) {
+            console.log(err);
+        }
+        console.log(result.rows);
+   })
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
